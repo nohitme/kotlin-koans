0163b64fb904e959f93ce21f5ee9c51f14506eec
Eric Lin <eric.lin@bottlerocketstudios.com>
2017-05-27 11:28:18 -0500

    Implement chapter 2

--- src/ii_collections/_21_Partition_.kt
+++ src/ii_collections/_21_Partition_.kt
@@ -12,5 +12,8 @@ fun example8() {
 
 fun Shop.getCustomersWithMoreUndeliveredOrdersThanDelivered(): Set<Customer> {
     // Return customers who have more undelivered orders than delivered
-    todoCollectionTask()
+    return this.customers.filter { customer ->
+        val (delivered, notDelivered) = customer.orders.partition { it.isDelivered }
+        return@filter notDelivered.count() > delivered.count()
+    }.toSet()
 }
