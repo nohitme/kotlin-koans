559699db54aaa555d1fd7417de93e784c9065418
Eric Lin <eric.lin@bottlerocketstudios.com>
2017-05-27 16:47:09 -0500

    Implement chapter 6

--- src/vi_generics/_41_GenericFunctions.kt
+++ src/vi_generics/_41_GenericFunctions.kt
@@ -20,11 +20,20 @@ fun task41(): Nothing = TODO(
 )
 
 fun List<String>.partitionWordsAndLines(): Pair<List<String>, List<String>> {
-    task41()
-//    return partitionTo(ArrayList<String>(), ArrayList()) { s -> !s.contains(" ") }
+    return partitionTo(ArrayList<String>(), ArrayList()) { s -> !s.contains(" ") }
+}
+
+fun <T, C : MutableCollection<T>> Collection<T>.partitionTo(one: C, two: C, predicate: (T) -> Boolean) : Pair<C, C> {
+    this.forEach {
+        if (predicate(it)) {
+            one.add(it)
+        } else {
+            two.add(it)
+        }
+    }
+    return one to two
 }
 
 fun Set<Char>.partitionLettersAndOtherSymbols(): Pair<Set<Char>, Set<Char>> {
-    task41()
-//    return partitionTo(HashSet<Char>(), HashSet()) { c -> c in 'a'..'z' || c in 'A'..'Z'}
+    return partitionTo(HashSet<Char>(), HashSet()) { c -> c in 'a'..'z' || c in 'A'..'Z'}
 }
