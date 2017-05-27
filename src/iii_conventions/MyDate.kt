9843f5fca6ec5779be2e13f39a62da2b5b7c2fbc
Eric Lin <eric.lin@bottlerocketstudios.com>
2017-05-27 12:38:49 -0500

    Implement chapter 3

--- src/iii_conventions/MyDate.kt
+++ src/iii_conventions/MyDate.kt
@@ -1,8 +1,21 @@
 package iii_conventions
 
-data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int)
+import iv_properties.toMillis
 
-operator fun MyDate.rangeTo(other: MyDate): DateRange = todoTask27()
+data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int) : Comparable<MyDate> {
+
+    override operator fun compareTo(that: MyDate): Int {
+        return if (this.toMillis() > that.toMillis()) {
+            1
+        } else if (this.toMillis() == that.toMillis()) {
+            0
+        } else {
+            -1
+        }
+    }
+}
+
+operator fun MyDate.rangeTo(other: MyDate): DateRange = DateRange(this, other)
 
 enum class TimeInterval {
     DAY,
@@ -10,4 +23,29 @@ enum class TimeInterval {
     YEAR
 }
 
-class DateRange(val start: MyDate, val endInclusive: MyDate)
+class DateRange(override val start: MyDate, override val endInclusive: MyDate): ClosedRange<MyDate>, Iterable<MyDate> {
+
+    override fun iterator(): Iterator<MyDate> {
+        return DateIterator(this)
+    }
+
+    override operator fun contains(date: MyDate): Boolean {
+        return start <= date && date <= endInclusive
+    }
+}
+
+class DateIterator(val dateRange: DateRange) : Iterator<MyDate> {
+
+    private var pointer = dateRange.start
+
+    override fun hasNext(): Boolean {
+        return pointer <= dateRange.endInclusive
+    }
+
+    override fun next(): MyDate {
+        val temp = pointer
+        pointer = pointer.nextDay()
+        return temp
+    }
+
+}
