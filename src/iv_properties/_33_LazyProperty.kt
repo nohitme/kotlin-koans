80868469826837719f88eb41ac44bf75abf55964
Eric Lin <eric.lin@bottlerocketstudios.com>
2017-05-27 12:51:57 -0500

    Implement chapter 4

--- src/iv_properties/_33_LazyProperty.kt
+++ src/iv_properties/_33_LazyProperty.kt
@@ -3,7 +3,14 @@ package iv_properties
 import util.TODO
 
 class LazyProperty(val initializer: () -> Int) {
-    val lazy: Int = todoTask33()
+    private var lazyVar : Int? = null
+    get() {
+        if (field == null) {
+            field = initializer()
+        }
+        return field
+    }
+    val lazy get() = lazyVar!!
 }
 
 fun todoTask33(): Nothing = TODO(
